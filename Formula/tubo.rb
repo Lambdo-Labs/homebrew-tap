40c6eddbb733a6dc2c7322c1e30cd534325459af
Andre Carrera <acarrera94@gmail.com>
2022-03-29 13:52:00 -0600

    Update tubo.rb

--- Formula/tubo.rb
+++ Formula/tubo.rb
@@ -2,7 +2,7 @@ class Tubo < Formula
   desc "Tubo localhost tunnel"
   homepage "https://github.com/Lambdo-Labs/tubo-releases"
   url "https://github.com/Lambdo-Labs/tubo-releases/releases/download/0.1.21/tubo-0.1.21.big_sur.bottle.tar.gz"
-  sha256 "7c64d7672aad2eb6acca5a185f20ec653e62458c17cebaac9b4c406414a6ea13"
+  sha256 "3c50b749de0124cc14b45a33c568b74d31bba016e83e867afac60e8c8a05f89a"
   version "0.1.21"
 
   def install
